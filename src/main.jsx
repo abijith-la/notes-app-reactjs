c52383995cde88632ac443a42a8f1ab9a80aba28
abijith-la <abijithla@gmail.com>
2023-08-22 12:36:55 +0530

    first edit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const page = (
+    <h1>REACT</h1>
 )
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(page)
+
+
